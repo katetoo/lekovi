5f164e026e0d2ff99247ab6481d3b47e1bffed62
Katerina Koceva <katerina_kcv@yahoo.com>
2016-01-21 16:03:42 +0100

    js and css changed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	
+    def is_active?(link_path)
+        current_page?(link_path) ? "active" : ""
+    end
+    
+    def extlink(link)
+        if link.include?("http://")
+            puts link
+        else
+            link.insert(0, "http://")
+            link
+        end
+    end
+    
 end
